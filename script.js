ab732f58bf0fa2a687c80264c0324cf6c4dae878
aditi <additigupta366@gmail.com>
2024-01-08 03:58:13 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed('.changing-text', {
-    strings: ["Frontend Developer","UI Designer","Canva"],
+    strings: ["Frontend Developer","UI Designer","Web Designer"],
     typeSpeed: 150,
     backSpeed:150,
     loop:true
